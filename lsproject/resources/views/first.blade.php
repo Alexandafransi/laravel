93af2d605f7293784aef105413ec6053bfbf91a8
Alexandafransi <alexismalecha@gmail.com>
2019-08-23 20:17:02 +0300

    i have add laravel start basic tutorial for create a project

---
+++ lsproject/resources/views/first.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+      
+    </head>
+    <body>
+        
+            <div class="content">
+                <div class="title m-b-md" style="text-align:center;margin-top:5%;background-color:whitesmoke;">
+                    AM ALEXANDA FRANSI AM GOING TO MAKE IT REALY 
+                </div>
+
+
+            </div>
+        
+    </body>
+</html>
